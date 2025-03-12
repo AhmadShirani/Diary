992e8c5d167e095d406666d5f5861d7d61c595f3
AhmadShirani <ah1sh2022@gmail.com>
2025-03-12 11:47:42 +0330

    Project update

---
+++ pages/login-page.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="bg-gradient-to-br from-zinc-900 to-red-500 w-screen h-screen flex items-center justify-center flex-col text-center">
+    <div class="grid gap-5 my-5">
+      <div class="border-b-4 border-white">
+        <h1 class="my-3 ml-60">ورود</h1>
+      </div>
+
+      <label class="ml-52">نام کاربری</label>
+      <input class="w-full bg-transparent placeholder:text-white text-white text-sm border border-white rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow">
+      <label class="ml-52">رمز عبور</label>
+      <input class="w-full bg-transparent placeholder:text-white text-white text-sm border border-white rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow">
+    </div>
+    <div class="mt-12">
+      <button class="bg-white text-black font-bold py-2 px-14 rounded-full">ورود</button>
+    </div>
+    <div class="absolute flex bottom-6 left-6 text-white px-2 py-2">
+      <p>قبلی</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+*
+{
+  direction: rtl;
+}
+</style>
